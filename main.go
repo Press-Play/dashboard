92e2c87b768a35e8df082595d78001b2a4eb99ef
Khanh Nguyen <khanh.phonic.nguyen@gmail.com>
2020-07-05 22:28:53 +0800

    connect with google calendar api and get the data

--- main.go
+++ main.go
@@ -3,16 +3,20 @@ package main
 import (
     "log"
     "os"
+    "time"
+    "fmt"
     "net/http"
     "html/template"
     "github.com/adlio/trello"
     "github.com/joho/godotenv"
+    "github.com/press-play/dashboard/calendar"
+    googlecalendar "google.golang.org/api/calendar/v3"
 )
 
-func doneHandler(client *trello.Client, w http.ResponseWriter, r *http.Request) {
+func doneHandler(clients Clients, w http.ResponseWriter, r *http.Request) {
     id := r.FormValue("id")
 
-    card, err := client.GetCard(id, trello.Defaults())
+    card, err := clients.trelloClient.GetCard(id, trello.Defaults())
     if err != nil {
         log.Fatal(err)
     }
@@ -23,9 +27,9 @@ func doneHandler(client *trello.Client, w http.ResponseWriter, r *http.Request) 
     http.Redirect(w, r, "/", http.StatusFound)
 }
 
-func dashboardHandler(client *trello.Client, w http.ResponseWriter, r *http.Request) {
+func dashboardHandler(clients Clients, w http.ResponseWriter, r *http.Request) {
     // Get the list of next tasks.
-    list, err := client.GetList("5a964a9ef272819fb87ebb15", trello.Defaults())
+    list, err := clients.trelloClient.GetList("5a964a9ef272819fb87ebb15", trello.Defaults())
     if err != nil {
         log.Fatal(err)
     }
@@ -42,7 +46,7 @@ func dashboardHandler(client *trello.Client, w http.ResponseWriter, r *http.Requ
     if id == "" {
         selectedCard = cards[0]
     } else {
-        selectedCard, err = client.GetCard(id, trello.Defaults())
+        selectedCard, err = clients.trelloClient.GetCard(id, trello.Defaults())
         if err != nil {
             log.Fatal(err)
         }
@@ -61,6 +65,28 @@ func dashboardHandler(client *trello.Client, w http.ResponseWriter, r *http.Requ
         log.Printf("action data:\n\t%+v", action.Data)
     }
 
+    // Load the calendar data.
+    tnow := time.Now().Local().Truncate(time.Hour * 24)
+    tmin := tnow.Format(time.RFC3339)
+    tmax := tnow.AddDate(0, 0, 1).Format(time.RFC3339)
+    events, err := clients.calendarClient.Events.List("primary").ShowDeleted(false).
+        SingleEvents(true).TimeMin(tmin).TimeMax(tmax).OrderBy("startTime").Do()
+    if err != nil {
+        log.Fatalf("Unable to retrieve next ten of the user's events: %v", err)
+    }
+    fmt.Println("Upcoming events:")
+    if len(events.Items) == 0 {
+        fmt.Println("No upcoming events found.")
+    } else {
+        for _, item := range events.Items {
+            date := item.Start.DateTime
+            if date == "" {
+                    date = item.Start.Date
+            }
+            fmt.Printf("%v (%v)\n", item.Summary, date)
+        }
+    }
+
     data := struct {
         Title string
         Tasklist []*trello.Card
@@ -74,9 +100,31 @@ func dashboardHandler(client *trello.Client, w http.ResponseWriter, r *http.Requ
     t.Execute(w, data)
 }
 
+func getEnv(key string, defaultValue string) string {
+    if value, exists := os.LookupEnv(key); exists {
+        return value
+    }
+
+    return defaultValue
+}
+
 type TrelloConfig struct {
     AppKey string
     Token  string
+}
+
+type Clients struct {
+    trelloClient  *trello.Client
+    calendarClient  *googlecalendar.Service
+}
+
+type ClientHandler struct {
+    clients Clients
+    handler func(Clients, http.ResponseWriter, *http.Request)
+}
+
+func (h *ClientHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    h.handler(h.clients, w, r)
 }
 
 func getTrelloConfig() *TrelloConfig {
@@ -86,21 +134,11 @@ func getTrelloConfig() *TrelloConfig {
     }
 }
 
-func getEnv(key string, defaultValue string) string {
-    if value, exists := os.LookupEnv(key); exists {
-        return value
+func NewClients(trelloClient  *trello.Client, calendarClient  *googlecalendar.Service) Clients {
+    return Clients {
+        trelloClient: trelloClient,
+        calendarClient: calendarClient,
     }
-
-    return defaultValue
-}
-
-type TrelloHandler struct {
-    client  *trello.Client
-    handler func(*trello.Client, http.ResponseWriter, *http.Request)
-}
-
-func (h *TrelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    h.handler(h.client, w, r)
 }
 
 func main() {
@@ -120,13 +158,17 @@ func main() {
     trelloClient := trello.NewClient(trelloConfig.AppKey, trelloConfig.Token)
     log.Printf("trello client:\n\t%+v", trelloClient)
 
+    // Initiate Calendar client.
+    calendarClient := calendar.GetClient()
+    log.Printf("calendar client:\n\t%+v", calendarClient)
+
     // Handle routes.
-    http.Handle("/", &TrelloHandler{trelloClient, dashboardHandler})
-    http.Handle("/done/", &TrelloHandler{trelloClient, doneHandler})
+    clients := NewClients(trelloClient, calendarClient)
+    http.Handle("/", &ClientHandler{clients, dashboardHandler})
+    http.Handle("/done/", &ClientHandler{clients, doneHandler})
     http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir(""))))
 
     // Serve the webpage and listen for requests.
     log.Print("http://localhost:8080")
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
